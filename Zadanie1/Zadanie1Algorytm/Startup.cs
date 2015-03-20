e26d7bc333c19478b56f05c99bf0b9056ee55b54
ZwanyPrzybyszem <przybysz20@gmail.com>
2015-03-20 04:23:18 +0100

    Utworzenie projektu Azure.

---
+++ Zadanie1/Zadanie1Algorytm/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Zadanie1Algorytm.Startup))]
+namespace Zadanie1Algorytm
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
