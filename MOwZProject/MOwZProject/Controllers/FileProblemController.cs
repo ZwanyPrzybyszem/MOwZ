31a100512fde5e09b9715f02980f6672d72f55dd
jajulia <jajulkaa@o2.pl>
2015-04-25 19:37:03 +0200

    sprzatanie

---
+++ MOwZProject/MOwZProject/Controllers/FileProblemController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MOwZProject.Models;
+
+namespace MOwZProject.Controllers
+{
+    public class FileProblemController : Controller
+    {
+        /// <summary>
+        /// GET dla wczytywania pliku
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult FileProblem()
+        {
+            return View(new FileProblem());
+        }
+
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="model">Obiekt zawierający zawartość pliku oraz obiekt reprezentujący problem</param>
+        /// <returns></returns>
+        [HttpPost]
+        public ActionResult FileProblem(FileProblem model)
+        {
+            try
+            {
+                model.updateProblem();
+                model.ProblemFromFile.getStillResult();
+            }
+            catch (Exception ex)
+            {
+                ViewBag.Message = "ERROR:" + ex.Message.ToString();
+            }
+            return View(model);
+        }
+        
+    }
+}
---
+++ MOwZProject/MOwZProject/Controllers/ProblemController.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MOwZProject.Models;
+
+namespace MOwZProject.Controllers
+{
+    public class ProblemController : Controller
+    {
+
+        /// <summary>
+        /// GET dla formularza
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Problem()
+        {
+            return View(new Problem());
+        }
+
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="Problem">Obiekt reprezentujący problem do przetworzenia</param>
+        /// <param name="add">Zawiera dane, gdy wybrano opcję dodania nowego stanu</param>
+        /// <param name="process">Zawiera dane, gdy wybrano opcję rozwiązania problemu</param>
+        /// <returns></returns>
+        [HttpPost]
+        public ActionResult Problem(Problem Problem, string add, string process)
+        {
+            try
+            {
+                ViewBag.Message = "";
+                if (add != null)
+                {
+                    Problem.States.Add(new State() { id = Problem.States.Count });
+                }
+                else
+                {
+                    if (Problem.ParlamentSize < 1)
+                    {
+                        throw new Exception(String.Format("Wpisz odpowiednią liczbę miejsc do przydziału! "));
+                    }
+                    int i = 0;
+                    foreach (var s in Problem.States)
+                    {
+                        s.id = i;
+
+                        if (s.Name == null || s.Name.Length < 0)
+                        {
+                            s.Name = (s.id + 1).ToString();
+                        }
+
+                        if (s.Size < 1)
+                        {
+                            throw new Exception(String.Format("Niepoprawne dane opisujące stany! Uzupełnij rozmiar stanu (liczba) odpowiednimi danymi!"));
+                        }
+                        i++;
+
+                    }
+                    Problem.getStillResult();
+                }
+            }
+            catch (Exception ex)
+            {
+                ViewBag.Message = "ERROR:" + ex.Message.ToString();
+            }
+            return View(Problem);
+        }
+
+
+
+    }
+}
